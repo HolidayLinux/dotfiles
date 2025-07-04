2c04ba6032d8c2aa39f8d229e2d19b0ef666b4b0
Holiday <alega00@mail.ru>
2025-07-04 10:14:14 +0800

    Change config files
    Update configs

--- .config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "vim", "vimdoc", "tsx", "python", "javascript", "html" },
-	sync_install = false,
-	auto_installed = true,
-	highlight = { enable = true },
-	indent = { enable = true },
-})
